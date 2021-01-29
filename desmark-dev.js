22661b32342251255457e389236c6a69ba2c3e6d
arnaud76-max <72136722+arnaud76-max@users.noreply.github.com>
2021-01-29 11:06:18 +0100

    Update desmark-dev.js

--- desmark-dev.js
+++ desmark-dev.js
@@ -133,6 +133,27 @@ function endLoadGost()
 	gost3.update(definitionLevel[level]);
 	gost4.update(definitionLevel[level]);
 	createPillules(definitionLevel[level].labyrinthe,definitionLevel[level].startX,definitionLevel[level].startY);
+	var nbrGost=4;
+	var gost = Array();
+	for(i=0 < nbrGost; i++){
+		var obj= new Object();
+		obj.x=5*tailleCelluleLaby;
+		obj.y=2*tailleCelluleLaby;
+		obj.direction=6,
+		obj.vitesse=2
+		obj.gestion=gestionGhost;
+		obj.ia=function(){};
+		obj.img=imageGost[0];
+		
+	}
+	gost[0].ia=gostImbecille;
+	gost[0].img[0];
+	gost[1].ia=gostMoinDebille;
+	gost[1].img[2];
+	gost[2].ia=gostImbecile;
+	gost[2].img[2];
+	gost[3].ia=gostMoinDebille;
+	gost[3].img[3];
 
 	loopMain();
 }
@@ -434,7 +455,58 @@ function createPillules(laby,x,y)
 	if(!(laby[y][x]&4) && !(laby[y+1][x]&(1<<4))) createPillules(laby,x,y+1);
 	if(!(laby[y][x]&8) && !(laby[y][x-1]&(1<<4))) createPillules(laby,x-1,y);
 }
- 
+ var gostImbecille =function(paramLevel)
+ {
+	if((this.x%tailleCelluleLaby)<this.vitesse && (this.y%tailleCelluleLaby)<this.vitesse) 
+	{
+		if(paramLevel.labyrinthe[parseInt(this.y/tailleCelluleLaby)][parseInt(this.x/tailleCelluleLaby)]&this.direction)
+		 {
+			while(paramLevel.labyrinthe[parseInt(this.y/tailleCelluleLaby)][parseInt(this.x/tailleCelluleLaby)]&this.direction) 
+			{
+				this.direction=2**Math.floor(Math.random()*4);
+			}
+		} 
+		else 
+		{
+			var demiTour=(this.direction<<2);if(demiTour>15) demiTour>>=4;
+			this.direction=2**Math.floor(Math.random()*4);
+			while(this.direction==demiTour || paramLevel.labyrinthe[parseInt(this.y/tailleCelluleLaby)][parseInt(this.x/tailleCelluleLaby)]&this.direction)
+			{
+				this.direction=2**Math.floor(Math.random()*4);
+			}
+		}
+	}
+//Directiopn par case du fantome//
+	switch(this.direction) 
+	{
+		case 1:
+		 this.y-=this.vitesse;
+		 break;
+		case 2:
+		 this.x+=this.vitesse;
+		 break;
+		case 4:
+		 this.y+=this.vitesse;
+		 break;
+		case 8:
+		 this.x-=this.vitesse;
+		 break;
+	}
+	canvasContext.save();
+		canvasContext.drawImage(imageGost[0],
+								0,0,tailleCelluleLaby,tailleCelluleLaby,
+								this.x,this.y,tailleCelluleLaby,tailleCelluleLaby);
+		canvasContext.restore();
+        var dx=this.x-desmarkDev.x;
+        var dy=this.y-desmarkDev.y;
+
+		if(((dx**2)+(dy**2)) < distanceCollision )
+		{
+		desmarkDev.mort=true;
+		document.getElementById("message").innerHTML="Desmark-Dev est mort";
+	
+   		}
+ }
 
 
 
